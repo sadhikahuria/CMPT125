54d79d12921dfabd748855edb0cdb2b660ff11bc
sadhika.huria <sadhikahuria@gmail.com>
2024-07-10 18:37:08 -0700

    up

--- labs/lab8/dataStructure.c
+++ labs/lab8/dataStructure.c
@@ -253,7 +253,7 @@ intArrayResult_t intArray_print( intArray_t * ia ) {
     printf("%d, ", ia -> data[i]);
   }
 
-  printf("%d\n", ia-> elementCount);
+  printf("%d\n", ia-> data[ia->elementCount]);
   
   return INTARR_OK;  // You are free to modify this return statement.
 }
