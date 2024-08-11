0e0937e892a3d01123e56ba1c794ab09a8a927a3
sadhika.huria <sadhikahuria@gmail.com>
2024-08-11 15:01:04 -0700

    ok

--- lectures/lab11/List.cpp
+++ lectures/lab11/List.cpp
@@ -14,3 +14,61 @@ List::~List(){
     }
 }
 
+unsigned int List::getElementCount() const{
+    return elementCount;
+}
+
+bool List::prepend( int newElement){
+    bool result = true;
+    Node *newNode = new Node(newElement);
+    if (newNode != nullptr){
+        if (tail == nullptr) tail = newNode;
+        if (head != nullptr) newNode-> next;
+        head = newNode;
+        elementCount++;
+    }
+    else result = false;
+
+    return result;
+}
+
+bool List::removeAtFront(){
+    bool result = true;
+
+    if ( head != nullptr){
+
+        Node * remove = head;
+        head = head->next;
+        remove -> next = nullptr;
+        delete remove;
+        remove = nullptr;
+
+        if ( head == nullptr ) tail = nullptr;
+
+        elementCount--;
+    }
+    else result = false;
+
+    return result;
+}
+
+bool List::getFrontElement(int *frontElement) const {
+    bool result = true;
+    if ( head != nullptr){
+        *frontElement = head -> data;
+    }
+    else result = false;
+    return result;
+}
+
+void List::printList() const{
+    cout << endl << "This List had " << elementCount << " elements:";
+    cout << "{ ";
+    Node *current = head;
+    while ( current != nullptr ){
+        cout << current->data << ", ";
+        current = current-> next;
+    }
+    cout << " }" << endl;
+    return;
+}
