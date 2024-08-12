e2f3cbcee229c741d5577626c8b7f47f94a6fede
sadhika huria <sadhikahuria@gmail.com>
2024-08-11 23:26:25 -0700

    hello

--- lectures/random.c
+++ lectures/random.c
@@ -2,18 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    int array[6];
-    array[0] = 5;
-    array[3] = 4;
-    array[5] = 3;
-    for (int i = 0; i < 6; i++){
-        if (array[i] != '\0'){
-            printf("yes ");
-        }
-        else{
-            printf("no ");
-        }
-    }
+int main(int argc, char *argv[]) {
+  if (12 == ((int) argv[1])){
+    printf("ok\n");
+  }
+  prinf("printing 2 and 3rd: %s and %s", argv[2], argv[3]);
+  
+  
+  return 0;
 }
